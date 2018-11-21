4124504d0a91c3980585e359db387e7b0cd6f176
gurcuesra <gurcuesra@gmail.com>
2018-11-21 10:28:14 -0800

    JavaProject

---
+++ 1-My First Program/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My Name is Mike Smith");
+        System.out.print("How Are you?");
+        System.out.println(" I am good/ Thanks");
+        System.out.println("Bye.Class is finished");
+        
+
+    }
+
+}
---
+++ 12-Reference Types/src/ReferenceClass.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class ReferenceClass {
+
+	public static void main(String[] args) {
+		int myIntValue=10;
+		int anotherIntValue = myIntValue;
+		
+		System.out.println("myIntValue:" + myIntValue);
+		System.out.println("anotherIntValue:" + anotherIntValue);
+		
+		anotherIntValue = anotherIntValue+1;
+		
+		System.out.println("myIntValue:" + myIntValue);
+		System.out.println("anotherIntValue:" + anotherIntValue);
+		
+		System.out.println("---------------------");
+		
+		int[] myIntArray=new int[5];
+		int[] anotherArray=myIntArray;
+		
+		System.out.println("myIntArray:" + Arrays.toString(myIntArray));
+		System.out.println("anotherArray:" + Arrays.toString(anotherArray));
+		
+		anotherArray[0]=1;
+		
+		System.out.println("myIntArray:" + Arrays.toString(myIntArray));
+		System.out.println("anotherArray:" + Arrays.toString(anotherArray));
+
+	}
+
+}
